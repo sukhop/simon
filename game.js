b1ea8baeb97177de6967e7deb3631c308cf3306b
SukhOPGod <sukhbircing@gmail.com>
2022-05-08 22:41:45 +0530

    game created

---
+++ game.js
@@ -0,0 +1,85 @@
+let buttonColors = ["red","blue","green","yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keypress(function (e) {
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+
+function nextSequence() {
+
+    userClickedPattern = [];
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $('#' + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+
+    level++;
+    $("#level-title").text("Level " + level);
+}
+
+$(".btn").on('click', function () {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);    
+});
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        console.log("wrong");
+
+        let sound = new Audio("sounds/wrong.mp3");
+        sound.play();
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
